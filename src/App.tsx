e49f3ccada0c4d34748dcb3f794ee35218338029
Telescope <ehwlgns000@gmail.com>
2023-07-13 19:35:02 +0900

    3주차 과제

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Layout from "./pages/Layout";
+import ListPage from "./pages/ListPage";
+import DetailPage from "./pages/DetailPage";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route index element={<ListPage />} />
+          <Route path="/:id" element={<DetailPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
---
+++ src/apis/axiosInstance.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+import { ORGANIZATION, REPO } from "../constants/common";
+
+const baseURL = `https://api.github.com/repos/${ORGANIZATION}/${REPO}/issues`;
+const client = axios.create({
+  baseURL,
+  headers: {
+    Accept: "application/vnd.github+json",
+    "X-Github-Api-Version": "2022-11-28",
+    //Authorization: `Bearer ${process.env.REACT_APP_TOKEN}`,
+  },
+});
+
+export default client;
